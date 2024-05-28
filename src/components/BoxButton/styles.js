0e29e23f60d5610e40130fe57c372c3d33817796
Eduardo Oris <109776498+eduardooris@users.noreply.github.com>
2024-05-28 18:10:22 -0300

    Update styles.js

--- src/components/BoxButton/styles.js
+++ src/components/BoxButton/styles.js
@@ -1,14 +1,13 @@
 import { StyleSheet, Platform } from "react-native";
 import cssGlobal from "../../util/cssGlobal";
-import { correctFontSizeForScreen } from "../../util/FontSize";
 import { correctSize } from "../../util/Size";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: cssGlobal.grayLight,
-    padding: correctSize(3),
     borderRadius: correctSize(10),
+    padding: correctSize(3),
     ...Platform.select({
       ios: {
         shadowColor: "#000",
@@ -20,44 +19,5 @@ export const styles = StyleSheet.create({
         elevation: 5,
       },
     }),
-  },
-  containerButton: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    ...Platform.select({
-      ios: {
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.15,
-        shadowRadius: 1.5,
-      },
-      android: {
-        elevation: 5,
-      },
-    }),
-  },
-  button: {
-    paddingVertical: correctSize(18),
-    borderRadius: correctSize(7),
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: cssGlobal.branco,
-    flex: 1,
-    margin: correctSize(2),
-  },
-  containerAlerts: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: correctSize(150),
-    padding: correctSize(5),
-  },
-  txtButton: {
-    textAlign: "center",
-    fontSize: correctFontSizeForScreen(8),
-    fontFamily: "RobotoMedium",
-    color: cssGlobal.cinza,
-    marginRight: correctSize(10),
   },
 });
