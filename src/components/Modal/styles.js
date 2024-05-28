d8c896a1a50065baef3a4f016ea66518f65456e6
Eduardo Oris <109776498+eduardooris@users.noreply.github.com>
2024-05-28 18:11:32 -0300

    Update styles.js

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,46 +1,49 @@
-import { StyleSheet } from 'react-native';
-import cssGlobal from '../../util/cssGlobal';
-import { correctSize } from '../../util/Size';
+import { StyleSheet } from "react-native";
+import cssGlobal from "../../util/cssGlobal";
+import hexGlobal from "../../util/hexGlobal";
+import { correctSize } from "../../util/Size";
 export const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
-    position: 'absolute',
-    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    flexDirection: "row",
   },
   containerDark: {
-    position: 'absolute',
-    backgroundColor: '#000',
+    position: "absolute",
+    backgroundColor: "#000",
     opacity: 0.9,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   boxModal: {
     width: correctSize(350),
-    height: correctSize(230),
     backgroundColor: cssGlobal.branco,
     borderRadius: correctSize(25),
   },
   boxModalPadding: {
     backgroundColor: cssGlobal.grayLight,
-    flex: 1,
     margin: correctSize(10),
     borderRadius: correctSize(25),
     paddingBottom: correctSize(25),
     paddingHorizontal: correctSize(20),
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   containerButton: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   title: {
     fontSize: correctSize(17),
+    fontFamily: "RobotoBold",
+    color: hexGlobal.hexPrim,
     marginTop: correctSize(20),
+    marginBottom: correctSize(10)
   },
   text: {
     fontSize: correctSize(13),
+    fontFamily: "RobotoRegular",
     color: cssGlobal.cinza,
     height: correctSize(140),
   },
